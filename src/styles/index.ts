8f12c7b89eca67c3293ed8b154789228a2b4df11
eugustavo <gustavo.ifto@gmail.com>
2020-11-21 23:15:56 -0300

    chore: Create styles to be based on creation

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+export default {
+  texts: {
+    bold: 'Poppins_500Medium',
+    regular: 'Poppins_400Regular',
+    light: 'Poppins_200ExtraLight'
+  },
+
+  colors: {
+    background: '#121212',
+    text: '#dbdbdb',
+    searchBackground: '#2e2e2e',
+    inputTextPlaceholder: '#7a7a7a',
+  }
+}
